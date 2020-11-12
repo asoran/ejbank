cfe0c61d87f0cef3af214ff083c749f4e278424d
mickael.boudignot <mickael.boudignot@192.168.1.17>
2020-11-12 21:56:27 +0100

    Add new WS examples

--- EJBank-web/src/main/java/com/ejbank/api/Test.java
+++ EJBank-web/src/main/java/com/ejbank/api/Test.java
@@ -1,10 +1,14 @@
 package com.ejbank.api;
 
+import com.ejbank.api.payload.PeoplePayload;
 import com.ejbank.test.TestBeanLocal;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -20,5 +24,18 @@ public class Test {
     @Path("/ejb")
     public String testEJB() {
         return testBean.test();
+    }
+    
+    @GET
+    @Path("/people/{age}")
+    public PeoplePayload testPayloadReponse(@PathParam("age") Integer age) {
+        return new PeoplePayload("Jean", "Dupont", age);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/post")
+    public String testPostRequest(PeoplePayload payload) {
+        return String.format("%s - %s", payload.getFirstname(), payload.getLastname());
     }
 }
