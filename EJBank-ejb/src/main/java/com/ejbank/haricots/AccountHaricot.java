8d4cc107100a30a05e2bffec43843b65e65a0203
Caucorico <guillaume.cau77@gmail.com>
2020-12-11 18:01:06 +0100

    feat : Create first Bean

---
+++ EJBank-ejb/src/main/java/com/ejbank/haricots/AccountHaricot.java
@@ -0,0 +1,34 @@
+package com.ejbank.haricots;
+
+import com.ejbank.entities.Account;
+import com.ejbank.entities.Customer;
+import com.ejbank.entities.User;
+import com.ejbank.repositories.AccountRepository;
+import com.ejbank.repositories.CustomerRepository;
+
+import javax.ejb.Local;
+import javax.inject.Inject;
+import java.util.List;
+
+@Local
+public class AccountHaricot {
+
+    @Inject
+    private AccountRepository accountRepository;
+
+    @Inject
+    private CustomerRepository customerRepository;
+
+    public List<Account> getAccountsByUserId(int userId) {
+        Customer customer = customerRepository.getById(userId);
+        return customer.getAccounts();
+    }
+
+    public Customer getAccountsAttachedByUserId(int userId) {
+
+    }
+
+    public Customer getCompleteAccountsByUserId(int userId) {
+
+    }
+}
