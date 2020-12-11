dc4a821e0e629f883a990d1b9b9984e85ce57b4f
Caucorico <guillaume.cau77@gmail.com>
2020-12-11 18:00:23 +0100

    feat : Add accounts list in Customer

--- EJBank-ejb/src/main/java/com/ejbank/entities/Customer.java
+++ EJBank-ejb/src/main/java/com/ejbank/entities/Customer.java
@@ -1,9 +1,7 @@
 package com.ejbank.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "ejbank_advisor")
@@ -12,6 +10,9 @@ public class Customer extends User {
     @ManyToOne(targetEntity = Advisor.class)
     @JoinColumn(name="advisor_id", nullable=true) /* todo : pass to false when possible */
     private Advisor advisor;
+
+    @OneToMany( mappedBy = "customer" )
+    private List<Account> accounts;
 
     public Customer(String login, String password, String email, String firstname, String lastname, Advisor advisor) {
         this.login = login;
@@ -25,5 +26,9 @@ public class Customer extends User {
 
     public Advisor getAdvisor() {
         return advisor;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
     }
 }
