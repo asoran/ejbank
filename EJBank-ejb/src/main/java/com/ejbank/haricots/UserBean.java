74e8637609b43bab3001e6b5b494f0c4941a5192
durand <durand@localhost>
2021-01-10 00:33:08 +0100

    add func

--- EJBank-ejb/src/main/java/com/ejbank/haricots/UserBean.java
+++ EJBank-ejb/src/main/java/com/ejbank/haricots/UserBean.java
@@ -1,11 +1,11 @@
 package com.ejbank.haricots;
 
-import com.ejbank.entities.User;
-import com.ejbank.repositories.UserRepository;
-
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+
+import com.ejbank.entities.User;
+import com.ejbank.repositories.UserRepository;
 
 @Stateless
 @LocalBean
@@ -13,9 +13,8 @@ public class UserBean implements Bean {
 
     @Inject
     private UserRepository userRepository;
-
+    
     public User getUserById(int id) {
-        return this.userRepository.getById(id);
+        return userRepository.getById(id);
     }
-
 }
--- EJBank-web/src/main/java/com/ejbank/api/UserServer.java
+++ EJBank-web/src/main/java/com/ejbank/api/UserServer.java
@@ -1,7 +1,10 @@
 package com.ejbank.api;
 
 import com.ejbank.api.payload.ServerPayload;
+import com.ejbank.entities.User;
+import com.ejbank.haricots.UserBean;
 
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,11 +14,19 @@ import javax.ws.rs.core.MediaType;
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
 public class UserServer {
-
+	
+	@EJB
+	private UserBean userBean;
+	
     @GET
     @Path("/{id}")
     public ServerPayload getStatus(@PathParam("age") int id) {
         return new ServerPayload(true);
+    }
+    @GET
+    @Path("/{user_id}")
+    public User getUserInfo(@PathParam("age") int id) {
+    	return userBean.getUserById(id);
     }
 
 }
